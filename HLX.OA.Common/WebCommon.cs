c917cdcb867980cfe23d530cf0fa8ef8a82a9894
Hlx <1003863495@qq.com>
2017-07-08 22:41:19 +0800

    2017/7/8

--- HLX.OA.Common/WebCommon.cs
+++ HLX.OA.Common/WebCommon.cs
@@ -1,5 +1,6 @@
 ﻿using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.PanGu;
+using PanGu;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -26,11 +27,26 @@ namespace HLX.OA.Common
             }
             return list;
         }
+        // /创建HTMLFormatter,参数为高亮单词的前后缀
+        public static string CreateHightLight(string keywords, string Content)
+        {
+            PanGu.HighLight.SimpleHTMLFormatter simpleHTMLFormatter =
+             new PanGu.HighLight.SimpleHTMLFormatter("<font color=\"red\">", "</font>");
+            //创建Highlighter ，输入HTMLFormatter 和盘古分词对象Semgent
+            PanGu.HighLight.Highlighter highlighter =
+            new PanGu.HighLight.Highlighter(simpleHTMLFormatter,
+            new Segment());
+            //设置每个摘要段的字符数
+            highlighter.FragmentSize = 150;
+            //获取最匹配的摘要段
+            return highlighter.GetBestFragment(keywords, Content);
+
+        }
 
 
 
 
 
-       
-     }
+
+    }
 }
--- HLX.OA.WenApp/Controllers/SearchController.cs
+++ HLX.OA.WenApp/Controllers/SearchController.cs
@@ -43,7 +43,7 @@ namespace HLX.OA.WenApp.Controllers
             return Content("ok");
         }
         public List<ViewModelContent> ShowSearchContent() {
-            string indexPath = @"F:\Program\OA\HLX.OA\HLX.OA.WenApp\lucenedir";
+            string indexPath = @"E:\GitProgram\HLX.OA.WenApp\lucenedir";
             List<string > kw = HLX.OA.Common.WebCommon.PanGuSplitWord(  Request["txtSearch"]);//对用户输入的搜索条件进行拆分。
        
 
@@ -66,10 +66,7 @@ namespace HLX.OA.WenApp.Controllers
                                                                                        //  this.listBox1.Items.Clear();
 
             List<ViewModelContent> vieModelList = new List<ViewModelContent>();
-
-
-
-
+            
             for (int i = 0; i < docs.Length; i++)
             {
                 ViewModelContent viewModel = new ViewModelContent();
@@ -79,7 +76,7 @@ namespace HLX.OA.WenApp.Controllers
                 Document doc = searcher.Doc(docId);//找到文档id对应的文档详细信息
                 viewModel.Id = Convert.ToInt32(doc.Get("Id"));
                 viewModel.Title = doc.Get("Title");
-                viewModel.Content = doc.Get("Content");
+                viewModel.Content = Common.WebCommon.CreateHightLight( Request["txtSearch"],doc.Get("Content"));//高亮显示 搜索内容
                 vieModelList.Add(viewModel);
             }
 
@@ -92,38 +89,36 @@ namespace HLX.OA.WenApp.Controllers
 
         private void CreateSearchContent()
          {
-            string indexPath = @"F:\Program\OA\HLX.OA\lucenedir";//注意和磁盘上文件夹的大小写一致，否则会报错。将创建的分词内容放在该目录下。  //配置文件 
-            FSDirectory directory = FSDirectory.Open(new DirectoryInfo(indexPath), new NativeFSLockFactory());//指定索引文件(打开索引目录) FS指的是就是FileSystem
-            bool isUpdate = IndexReader.IndexExists(directory);//IndexReader:对索引进行读取的类。该语句的作用：判断索引库文件夹是否存在以及索引特征文件是否存在。
-            if (isUpdate)
-            {
-                //同时只能有一段代码对索引库进行写操作。当使用IndexWriter打开directory时会自动对索引库文件上锁。
-                //如果索引目录被锁定（比如索引过程中程序异常退出），则首先解锁（提示一下：如果我现在正在写着已经加锁了，但是还没有写完，这时候又来一个请求，那么不就解锁了吗？这个问题后面会解决）
-                if (IndexWriter.IsLocked(directory))
-                {
-                    IndexWriter.Unlock(directory);
-                }
-            }
-            IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isUpdate, Lucene.Net.Index.IndexWriter.MaxFieldLength.UNLIMITED);//向索引库中写索引。这时在这里加锁。
-            List<Books> list = BooksService.LoadEntities(b => true).ToList();
-            foreach (var bookModel in list)
-            {
-                Document document = new Document();//表示一篇文档。
-                //Field.Store.YES :表示是否存储原值。只有当Field.Store.YES在后面才能用doc.Get("number")取出值来.Field.Index. NOT_ANALYZED:不进行分词保存
-                document.Add(new Field("Id", bookModel.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            //string indexPath = @"F:\Program\OA\HLX.OA\lucenedir";//注意和磁盘上文件夹的大小写一致，否则会报错。将创建的分词内容放在该目录下。  //配置文件 
+            //FSDirectory directory = FSDirectory.Open(new DirectoryInfo(indexPath), new NativeFSLockFactory());//指定索引文件(打开索引目录) FS指的是就是FileSystem
+            //bool isUpdate = IndexReader.IndexExists(directory);//IndexReader:对索引进行读取的类。该语句的作用：判断索引库文件夹是否存在以及索引特征文件是否存在。
+            //if (isUpdate)
+            //{
+            //    //同时只能有一段代码对索引库进行写操作。当使用IndexWriter打开directory时会自动对索引库文件上锁。
+            //    //如果索引目录被锁定（比如索引过程中程序异常退出），则首先解锁（提示一下：如果我现在正在写着已经加锁了，但是还没有写完，这时候又来一个请求，那么不就解锁了吗？这个问题后面会解决）
+            //    if (IndexWriter.IsLocked(directory))
+            //    {
+            //        IndexWriter.Unlock(directory);
+            //    }
+            //}
+            //IndexWriter writer = new IndexWriter(directory, new PanGuAnalyzer(), !isUpdate, Lucene.Net.Index.IndexWriter.MaxFieldLength.UNLIMITED);//向索引库中写索引。这时在这里加锁。
+            //List<Books> list = BooksService.LoadEntities(b => true).ToList();
+            //foreach (var bookModel in list)
+            //{
+            //    Document document = new Document();//表示一篇文档。
+            //    //Field.Store.YES :表示是否存储原值。只有当Field.Store.YES在后面才能用doc.Get("number")取出值来.Field.Index. NOT_ANALYZED:不进行分词保存
+            //    document.Add(new Field("Id", bookModel.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
 
-                //Field.Index. ANALYZED:进行分词保存:也就是要进行全文的字段要设置分词 保存（因为要进行模糊查询）
+            //    //Field.Index. ANALYZED:进行分词保存:也就是要进行全文的字段要设置分词 保存（因为要进行模糊查询）
 
-                //Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS:不仅保存分词还保存分词的距离。
-                document.Add(new Field("Title", bookModel.Title.ToString(), Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
+            //    //Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS:不仅保存分词还保存分词的距离。
+            //    document.Add(new Field("Title", bookModel.Title.ToString(), Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
 
-                document.Add(new Field("Content", bookModel.ContentDescription.ToString(), Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
+            //    document.Add(new Field("Content", bookModel.ContentDescription.ToString(), Field.Store.YES, Field.Index.ANALYZED, Lucene.Net.Documents.Field.TermVector.WITH_POSITIONS_OFFSETS));
             
-                writer.AddDocument(document);
+            //    writer.AddDocument(document);
             }
-
-
-
+            
            
             writer.Close();//会自动解锁。
             directory.Close();//不要忘了Close，否则索引结果搜不到
